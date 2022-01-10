fa93066c59cb9e88aef5057c376e6cd7acab0215
lfgberg <lfgaberg@gmail.com>
2022-01-10 14:51:27 -0500

    Initial Commit
    * established folder structure
    * add first setup lab

---
+++ labs/lab1/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+/*
+* Liam Geyer
+* IST242 - Lab1
+* lfg5289@psu.edu
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] nums = {10, 20, 30};
+
+	    for (int num : nums){
+	        System.out.println(num);
+        }
+    }
+}
