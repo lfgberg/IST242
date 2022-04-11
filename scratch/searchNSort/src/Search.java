6e86deffbb49246ea0099307dd9482b6e9cb3726
Liam Geyer <lfgaberg@gmail.com>
2022-04-11 15:25:26 -0400

    add notes on searching and sorting algorithms

--- scratch/searchNSort/src/Search.java
+++ scratch/searchNSort/src/Search.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Search {
+import java.util.List;
+
+/**
+ * Liam Geyer
+ * IST242 - Searching and Sorting alg notes
+ * lfg5289@psu.edu
+ */
+
+public class Search {
+    /**
+     * Searches linearly through a list, O(n)
+     * @param l list to search through
+     * @param x item to find
+     * @return index of x or -1
+     */
+    public static <T> int linearSearch(List<T> l, T x){
+        for (T y : l){
+            if (x == y){
+                return l.indexOf(x);
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Removes half of the list to search through each time O(log(n))
+     * @param l list to search through, must be in sorted order O(n) if you check this tho so don't?
+     * @param x item to find
+     * @return index of x or -1
+     */
+    public static <T extends Comparable<T>> int binarySearchRecursive(List<T> l, T x){  //  <-- kickoff
+        return binarySearchRecursive(l, x, 0, l.size() - 1); //  <--  initially searches the entire array
+    }
+
+    private static <T extends Comparable<T>> int binarySearchRecursive(List<T> l, T x, int lBound, int uBound){
+        //  stopping case
+        if (uBound < lBound){
+            return -1;
+        }
+
+        //  divide the list
+        int mid = (lBound + uBound) / 2;
+
+        if (x.equals(l.get(mid))){
+            return mid; //  <-- found it
+        } else if (x.compareTo(l.get(mid)) > 0){ // if key > arr
+            //  if the key comes after whatever's at the mid then remove the first half
+            return binarySearchRecursive(l, x, mid + 1, uBound);
+        } else {
+            //  key comes before mid, throw out the second half
+            return binarySearchRecursive(l, x, lBound, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> test = List.of(7, 22, 29, 32, 42, 52 , 59, 66, 69, 76);
+
+        //  testing binary sort (recursive)
+        System.out.println(binarySearchRecursive(test, 32)); // 3
+        System.out.println(binarySearchRecursive(test, 7)); // 0
+        System.out.println(binarySearchRecursive(test, 29)); // 2
+        System.out.println(binarySearchRecursive(test, 76)); // 7
+        System.out.println(binarySearchRecursive(test, 66)); //  9
+    }
 }
--- scratch/searchNSort/src/Sort.java
+++ scratch/searchNSort/src/Sort.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Sort {
+import java.util.*;
+
+/**
+ * Liam Geyer
+ * IST242 - Searching and Sorting alg notes
+ * lfg5289@psu.edu
+ */
+
+public class Sort {
+    /**
+     * Sorts a list of objects in ascending order; is destructive
+     * O(n^2)
+     * @param l list of type T
+     */
+    public static <T extends Comparable<T>> void insertionSort(List<T> l){
+        int i = 1;
+        while (i < l.size()){
+            int j = i;
+            while (j > 0 && l.get(j - 1).compareTo(l.get(j)) > 0){
+                Collections.swap(l, j, j -1 );
+                j--;
+            }
+            i++;
+        }
+    }
+
+    /**
+     * Sort a list of objects in ascending order recursively and quickly
+     * O(n * log (n))
+     * @param l list of type T
+     */
+    public static <T extends Comparable<T>> void mergeSort(List<T> l){ //  <-- kickoff
+
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(List<T> l, List<T> m){
+
+    }
+
+    public static void main(String[] args) {
+        //  testing insertionSort
+        List<Integer> test = Arrays.asList(6, 5, 3, 1, 8, 7, 2, 4);
+        insertionSort(test);
+        System.out.println(test);
+    }
 }
